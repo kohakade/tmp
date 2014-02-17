a7674bff70e4735c0eb155d21ddff967aba86519
kohakade <abhijeet.kohakade99@gmail.com>
2014-02-17 16:21:49 +0530

    Create tmp1.sh

---
+++ tmp1.sh
@@ -0,0 +1,11 @@
+!# /bin/bash
+
+sudo sed -i 's/127.0.0.1/172.16.0.1/g' /etc/glance/glance-registry-paste.ini
+
+sudo sed -i 's/%SERVICE_TENANT_NAME%/service/g' /etc/glance/glance-registry-paste.ini
+
+sudo sed -i 's/%SERVICE_USER%/glance/g' /etc/glance/glance-registry-paste.ini
+
+sudo sed -i 's/%SERVICE_PASSWORD%/glance/g' /etc/glance/glance-registry-paste.ini
+
+exit
